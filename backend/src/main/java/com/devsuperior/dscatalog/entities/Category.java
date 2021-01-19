11942bb60ed08c2c13e840cd5dd0064a78fda60f
Rogério Chagas <rogerio.chagas79@gmail.com>
2021-01-18 23:58:15 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "td_category")
+@Table(name = "tb_category")
 public class Category implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
